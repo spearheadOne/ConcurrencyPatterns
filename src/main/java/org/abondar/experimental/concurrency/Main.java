98ad3c71c591e105d14cbbc8dcaa585379917671
Alex Bondar <abondar1992@gmail.com>
2026-02-26 08:13:57 +0100

    logging setup

--- src/main/java/org/abondar/experimental/concurrency/Main.java
+++ src/main/java/org/abondar/experimental/concurrency/Main.java
@@ -1,7 +1,13 @@
 package org.abondar.experimental.concurrency;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println( "Hello World!");
+       log.info("Hello world");
     }
 }
